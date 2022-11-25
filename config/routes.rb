29cd1d9d1a7b4f8a8861641989aa3d5d3f190b3b
guillaume drevo <guillaume.drevo@gmail.com>
2022-11-25 12:26:07 +0100

    added index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :movies, only: %i[index show]
+  root "lists#index"
   resources :bookmarks, only: :destroy
   resources :lists, only: %i[index new create show destroy] do
     resources :bookmarks, only: %i[new create]
